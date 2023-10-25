04d2eda30efbf08d5b9963d62d2351d909b4cf44
Sergio Serrano <sergioandresgmd10@gmail.com>
2023-10-25 16:28:42 -0500

    feat: new route

--- src/index.js
+++ src/index.js
@@ -8,6 +8,9 @@ connectDb()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use("/", productsRouter)
+app.get("/", (req, res) => {
+  res.send("Hello World!")
+})
 app.listen(process.env.PORT, () => {
   console.log("Example app listening on port 3000!")
 })
