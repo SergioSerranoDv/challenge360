b6fac8ef92af2b0d4df88a39e0a1c4ddf58745ab
Sergio Serrano <sergioandresgmd10@gmail.com>
2023-10-25 17:28:03 -0500

    feat: new description for the pricipal  route

--- index.js
+++ index.js
@@ -9,7 +9,15 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use("/", productsRouter)
 app.get("/", (req, res) => {
-  res.send("Hello World!")
+  res.send({
+    instructions: "💻 Hi this is the API 1.0.0 for the NodeJS challange ",
+    endpoints: {
+      products: "/products",
+      prices: "/products/:user_id/:nombre_producto",
+    },
+    author: "Sergio Serrano",
+    repository: "🌐https://github.com/SergioSerranoDv/challenge360",
+  })
 })
 app.listen(process.env.API_PORT, () => {
   console.log(`Example app listening on port ${process.env.API_PORT}`)
