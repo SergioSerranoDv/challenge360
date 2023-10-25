c32ae9bbaadf76f112cad68bfcbfb8ac231e4728
Sergio Serrano <sergioandresgmd10@gmail.com>
2023-10-25 17:04:16 -0500

    fix: updated to API_PORT

--- index.js
+++ index.js
@@ -11,6 +11,6 @@ app.use("/", productsRouter)
 app.get("/", (req, res) => {
   res.send("Hello World!")
 })
-app.listen(process.env.PORT, () => {
-  console.log("Example app listening on port 3000!")
+app.listen(process.env.API_PORT, () => {
+  console.log(`Example app listening on port ${process.env.API_PORT}`)
 })
